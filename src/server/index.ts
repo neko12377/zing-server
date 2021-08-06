f630e94bc293513d6d2fc4f79f72cd847bb702b4
Kasper Chen <neko12377@users.noreply.github.com>
2021-08-06 18:29:11 +0800

    feat: add server

---
+++ src/server/index.ts
@@ -0,0 +1,9 @@
+import {IncomingMessage, ServerResponse} from "http";
+
+const http = require("http");
+
+const requestListener =  (req: IncomingMessage, res: ServerResponse) => {
+
+}
+
+http.createServer();
